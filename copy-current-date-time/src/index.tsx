c34a1b743f5b3ad1bf668fda992b228682a25c91
Tony Xiao <tonyx.ca@gmail.com>
2021-12-18 08:38:36 -0800

    Initial version of paste date time to clipboard

--- copy-current-date-time/src/index.tsx
+++ copy-current-date-time/src/index.tsx
@@ -1,5 +1,16 @@
-import { Detail } from "@raycast/api";
+import { closeMainWindow, copyTextToClipboard, pasteText, showHUD } from "@raycast/api";
+import { DateTime } from "luxon";
 
-export default function main() {
-  return <Detail markdown="# Hello World" />;
+export default async function pasteDateTimeToClipboard() {
+  // 20210911-1530 is the desired format
+  const dateStr = DateTime.local().toFormat("yyyyMMdd-hhmm ");
+  await pasteText(dateStr);
+  await closeMainWindow();
+}
+
+export async function copyDateTimeToClipboard() {
+  // Alternative
+  const dateStr = DateTime.local().toFormat("yyyyMMdd-hhmm ");
+  await copyTextToClipboard(dateStr);
+  await showHUD(`${dateStr} copied to clipboard`);
 }
