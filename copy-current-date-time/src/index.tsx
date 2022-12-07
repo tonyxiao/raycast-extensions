44a9ad006eca3a6463566ace99c46a25c272a956
Tony Xiao <tonyx.ca@gmail.com>
2022-12-07 14:24:45 +0100

    Update format

--- copy-current-date-time/src/index.tsx
+++ copy-current-date-time/src/index.tsx
@@ -3,14 +3,14 @@ import { DateTime } from "luxon";
 
 export default async function pasteDateTimeToClipboard() {
   // 20210911-1530 is the desired format
-  const dateStr = DateTime.local().toFormat("yyyyMMdd-hhmm ");
+  const dateStr = DateTime.local().toFormat("yyyy-MM-dd HH:mm ");
   await pasteText(dateStr);
   await closeMainWindow();
 }
 
 export async function copyDateTimeToClipboard() {
   // Alternative
-  const dateStr = DateTime.local().toFormat("yyyyMMdd-hhmm ");
+  const dateStr = DateTime.local().toFormat("yyyy-MM-dd HH:mm ");
   await copyTextToClipboard(dateStr);
   await showHUD(`${dateStr} copied to clipboard`);
 }
